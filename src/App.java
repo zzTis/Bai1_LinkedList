294ec0056265559788cdcd737e7cfc956038d288
Le Thanh Nguyen <155604610+zzTis@users.noreply.github.com>
2024-10-01 18:34:11 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("I love you");
+        System.out.println("I love you eeee");
     }
 
 }
