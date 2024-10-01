ae9e7d323f6914445d815fcb231021155c8902ff
zzTis <thnguyen0407@gmail.com>
2024-10-01 18:39:50 +0700

    Change3

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("I love you eeee");
+        System.out.println("I love you eeee đồ queee");
     }
 
 }
